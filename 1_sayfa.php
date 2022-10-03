976f52a076597b7a35b50df8e23d3facafd5db99
BerkayZAN <114944903+BerkayZAN@users.noreply.github.com>
2022-10-03 23:32:44 +0300

    Add files via upload

---
+++ 1_sayfa.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="tr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>1_sayfa </title>
+      
+</head>
+<body>
+     <h1> Toplama İşlemi</h1>
+    <form name="topislem">
+         Sayı 1: <input type="text" name="sayı1" placeholder="Bir Sayı Giriniz." required="required">  <br>
+         <br>
+         Sayı 2:  <input type="text" name="sayı2" placeholder="Bir Sayı Giriniz." required="required"> 
+    <br>
+    <br>
+    </form>
+    <div class="buttons">
+    
+        <button> Topla</button> 
+        
+
+    </div>
+</body>
+</html>
